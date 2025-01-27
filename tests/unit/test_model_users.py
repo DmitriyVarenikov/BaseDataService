98f3604c370295276b7be361e6c0c07abde62d4f
Varenikod Dmitry <varenikovdmitry@gmail.com>
2025-01-27 13:13:39 +0300

    feature(tests): расширен create для Users

--- tests/unit/test_model_users.py
+++ tests/unit/test_model_users.py
@@ -1,14 +1,17 @@
 import pytest
 
+from sqlalchemy.exc import IntegrityError
+
 from src.data_base.model import Users
 from src.data_base.service_model.base_service import BaseCRUDService
+from ..data.data_model_users import parametrize_create, parametrize_duplicate_name, parametrize_invalid_user_data
 
 
 # Успешное создание записи. Создание нескольких объектов. +
-# Обработку ошибок (некорректные данные, дублирование уникальных полей).
-# Работа с автоматически генерируемыми полями.
-# Возврат созданного объекта.
-# Обработку транзакций и откатов (rollback).
+# Обработку ошибок (некорректные данные, дублирование уникальных полей). +
+# Работа с автоматически генерируемыми полями. +
+# Возврат созданного объекта. +
+# Обработку транзакций и откатов (rollback). +
 
 @pytest.fixture
 def setup_users_table(db):
@@ -18,46 +21,76 @@ def setup_users_table(db):
     yield db
 
 
+@pytest.fixture
+def db_session(setup_users_table):
+    """
+    Создает фикстуру для сессии базы данных.
+
+    Для тестов коммит внутри сессии отключен.
+
+    :param setup_users_table: Фикстура для настройки таблицы пользователей.
+    """
+    with setup_users_table.session_scope(commit=False) as session:
+        yield session
+
+
 class TestUsersCRUDCreate:
 
     @pytest.mark.parametrize(
-        "users_data, expected_count",
-        [
-            # Тест на создание одной записи
-            ([{"nickname": "user1", "name": "Иван", "surname": "Иванов"}], 1),
+        "users_data, expected_count", parametrize_create)
+    def test_create(self, db_session, users_data, expected_count):
+        base_serv = BaseCRUDService(db_session, Users)
+        for user_data in users_data:
+            new_users = base_serv.create(**user_data)
+            assert new_users.nickname == user_data["nickname"]
+            assert new_users.name == user_data["name"]
+            assert new_users.surname == user_data["surname"]
 
-            # Тест на создание нескольких записей
-            ([
-                 {"nickname": "user1", "name": "Иван", "surname": "Иванов"},
-                 {"nickname": "user2", "name": "Алексей", "surname": "Петров"},
-                 {"nickname": "user3", "name": "Мария", "surname": "Сидорова"}
-             ], 3),
-        ]
-    )
-    def test_create(self, setup_users_table, users_data, expected_count):
+        db_session.flush()
+
+        saved_users = db_session.query(Users).all()
+        assert len(saved_users) == expected_count
+
+        for user_data in users_data:
+            saved_user = db_session.query(Users).filter_by(nickname=user_data["nickname"]).first()
+            assert saved_user is not None
+            assert saved_user.name == user_data["name"]
+            assert saved_user.surname == user_data["surname"]
+            assert saved_user.id is not None, "Поле id должно быть сгенерировано"
+            assert saved_user.created_at is not None, "Поле created_at должно быть сгенерировано"
+
+    @pytest.mark.parametrize("users_data", parametrize_duplicate_name)
+    def test_create_duplicate_nickname(self, db_session, users_data):
         """
-        Тестирует создание пользователей в базе данных.
+        Тест проверяет создание записи пользователя с дублирующимся никнеймом.
 
-        :param setup_users_table: Фикстура для инициализации таблицы пользователей.
-        :param users_data: Список данных пользователей для создания записей в таблице.
-        :param expected_count: Ожидаемое количество записей в таблице после создания.
-        :raises AssertionError: Если количество созданных записей или данные создаваемых пользователей не соответствуют ожидаемым.
+        :param users_data: Данные пользователей, включающие оригинальный и дублирующийся никнейм.
         """
-        with setup_users_table.session_scope() as session:
-            base_serv = BaseCRUDService(session, Users)
-            for user_data in users_data:
-                new_users = base_serv.create(**user_data)
-                assert new_users.nickname == user_data["nickname"]
-                assert new_users.name == user_data["name"]
-                assert new_users.surname == user_data["surname"]
+        base_serv = BaseCRUDService(db_session, Users)
+        base_serv.create(**users_data[0])
+        db_session.flush()
 
-            session.commit()
+        # проверяем, что первый пользователь сохранён
+        saved_users = db_session.query(Users).all()
+        assert len(saved_users) == 1  # сохраняем только уникальные записи
+        assert saved_users[0].nickname == users_data[0]["nickname"]
 
-            saved_users = session.query(Users).all()
-            assert len(saved_users) == expected_count
+        # Проверяем, что создание дубликата вызывает IntegrityError
+        with pytest.raises(IntegrityError):
+            base_serv.create(**users_data[1])
+            db_session.flush()
 
-            for user_data in users_data:
-                saved_user = session.query(Users).filter_by(nickname=user_data["nickname"]).first()
-                assert saved_user is not None
-                assert saved_user.name == user_data["name"]
-                assert saved_user.surname == user_data["surname"]
+    @pytest.mark.parametrize("users_data", parametrize_invalid_user_data)
+    def test_invalid_user_data(self, db_session, users_data):
+        """
+        Тест проверяет обработку некорректных данных пользователей и выбрасывание исключений.
+
+        :param db_session: Сессия базы данных.
+        :param users_data: Набор данных с некорректной информацией о пользователях.
+        """
+        base_serv = BaseCRUDService(db_session, Users)
+        for user_data in users_data:
+            with pytest.raises(IntegrityError):
+                base_serv.create(**user_data)
+                db_session.flush()
+            db_session.rollback()
