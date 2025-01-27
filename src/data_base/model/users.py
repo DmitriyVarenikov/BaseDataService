d62d4a86edcf647cb0fbdb7cf21b73089d977bb0
Varenikod Dmitry <varenikovdmitry@gmail.com>
2025-01-27 13:04:33 +0300

    feature(model): добавлен CheckConstraint для Users

--- src/data_base/model/users.py
+++ src/data_base/model/users.py
@@ -1,8 +1,9 @@
 from sqlalchemy.sql import func
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, CheckConstraint
 
 from .base_model import BaseModel
 from ..configuration.constrains import TableName
+
 
 class Users(BaseModel):
     __tablename__ = TableName.USERS.value
@@ -12,6 +13,12 @@ class Users(BaseModel):
     created_at = Column(DateTime, default=func.now(), nullable=False)
     name = Column(String(50), nullable=False)
     surname = Column(String(50), nullable=False)
+
+    __table_args__ = (
+        CheckConstraint("nickname <> ''", name="check_nickname_not_empty"),
+        CheckConstraint("name <> ''", name="check_name_not_empty"),
+        CheckConstraint("surname <> ''", name="check_surname_not_empty"),
+    )
 
     def __repr__(self) -> str:
         return f"{TableName.USERS.value}(id={self.id}, nickname={self.nickname}, created_at={self.created_at}, name={self.name}, surname={self.surname})"
