be34b0decfb848be97bdcdbeffa61e9fda9806fa
Varenikod Dmitry <varenikovdmitry@gmail.com>
2025-02-04 10:46:43 +0300

    refactor: улучшена типизация и валидация в BaseCRUDService

--- src/data_base/service_model/base_service.py
+++ src/data_base/service_model/base_service.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
-from typing import Type, TypeVar, Generic, Optional, Any
-
+from typing import Type, TypeVar, Generic, Optional, Any, Union
+from sqlalchemy.sql.expression import BinaryExpression
 from src.data_base.model.base_model import BaseModel
 from sqlalchemy import and_, or_
 
@@ -40,51 +40,73 @@ class BaseCRUDService(Generic[T]):
         deleted_count = query.delete(synchronize_session="fetch")
         return deleted_count
 
-    def update(self, filters: dict, updates: dict) -> int:
+    def update(self, filters: list[Any], updates: dict, use_or: bool = False, ) -> int:
         """
-        Обновляет записи, соответствующие фильтрам, новыми значениями.
+        Обновляет записи в базе данных на основе заданных условий и значений для обновления.
 
-        :param filters: Словарь с условиями для выбора записей.
-        :param updates: Словарь с полями и их новыми значениями.
-        :return: Количество обновлённых записей.
+        :param filters: Список условий фильтрации SQLAlchemy.
+        :param updates: Словарь с данными для обновления.
+        :param use_or: Если True, применяет логическое "или" к фильтрам; иначе применяет "и". По умолчанию False.
+        :raises TypeError: Если filters не является списком, updates не является словарём или filters содержит элементы, не являющиеся SQLAlchemy условиями фильтрации.
+        :raises ValueError: Если filters пуст или updates не содержит данных.
         """
+        if not isinstance(filters, list):
+            raise TypeError(f"filters должен быть списком, а не {type(filters).__name__}")
+
+        if not filters:
+            raise ValueError("Не переданы условия фильтрации")
+
+        if not all(isinstance(f, BinaryExpression) for f in filters):
+            raise TypeError("filters должен содержать только условия фильтрации SQLAlchemy")
+
+        if not isinstance(updates, dict):
+            raise TypeError(f"updates должен быть словарём, а не {type(updates).__name__}")
+
         if not updates:
-            raise ValueError("")
+            raise ValueError("Не переданы данные для обновления")
 
-        query = self._session.query(self._model).filter_by(**filters)
+        query = self._session.query(self._model)
+        query = query.filter(or_(*filters) if use_or else and_(*filters))
         updated_count = query.update(updates, synchronize_session="fetch")
         return updated_count
 
     def read(self,
-             filters: Optional[list[Any]] = None,
+             filters: Optional[list[BinaryExpression]] = None,
              use_or: bool = False,
-             order_by=None,
+             order_by = None,
              limit: int = None,
              offset: int = None) -> list[T]:
         """
-        Читает данные из базы данных на основе заданных фильтров и параметров сортировки.
+        Читает данные из базы данных с использованием заданных фильтров, сортировки, лимита и смещения.
 
-        :param filters: Список фильтров для применения к запросу.
-        :param use_or: Логическое значение, определяющее использование логического "ИЛИ" вместо "И" для фильтров.
-        :param order_by: Поле или список полей для сортировки результатов.
-        :param limit: Максимальное количество возвращаемых записей.
-        :param offset: Смещение количества пропускаемых записей от начала.
+        :param filters: Список условий фильтрации SQLAlchemy, опционально.
+        :param use_or: Определяет, использовать ли логическое "ИЛИ" вместо "И" при применении фильтров. По умолчанию False.
+        :param order_by: Порядок сортировки. Может быть одиночным условием или списком условий.
+        :param limit: Максимальное количество возвращаемых записей. Опционально.
+        :param offset: Смещение записей. Опционально.
+        :raises TypeError: Если переданные фильтры не являются списком или содержат некорректные условия.
         """
         query = self._session.query(self._model)
 
         if filters:
+            if not isinstance(filters, list):
+                raise TypeError(f"filters должен быть списком, а не {type(filters).__name__}")
+
+            if not all(isinstance(f, BinaryExpression) for f in filters):
+                raise TypeError("filters должен содержать только условия фильтрации SQLAlchemy")
+
             query = query.filter(or_(*filters) if use_or else and_(*filters))
 
-        if order_by:
+        if order_by is not None:
             if isinstance(order_by, list):
                 query = query.order_by(*order_by)
             else:
                 query = query.order_by(order_by)
 
-        if limit:
+        if limit is not None:
             query = query.limit(limit)
 
-        if offset:
+        if offset is not None:
             query = query.offset(offset)
 
         return query.all()
